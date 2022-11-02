6c531829b7099c02d5af438a5ef469159415f2de
GathunguAlex <alex.gathungu@student.moringaschool.com>
2022-11-03 00:55:09 +0300

    initializing,saving in db

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,59 @@
+
 class Song
 
-  attr_accessor :name, :album
+  #Song
+  #when initialized with a name and a albun
+   # the name attribute can be accessed
+    #the album attribute can be accessed
+    #sets the initial value of id to nil
 
-  def initialize(name:, album:)
+  attr_accessor :name, :album, :id
+
+  def initialize(name:, album:, id: nil)
+    @id = id
     @name = name
     @album = album
+  end
+      #.create_table
+    #creates the songs table in the database 
+  
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
+   
+     #save
+    #saves an instance of the Song class to the database
+    #assigns the id of the song from the database to the instance
+    #returns the new object that it instantiated
+
+
+  def save
+    sql = <<-SQL
+      INSERT INTO songs (name, album)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.album)
+    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+
+    self
+
+  end
+
+
+#.create
+#    saves a song to the database
+#   returns the new object that it instantiated
+  def self.create(name:, album:)
+    song = Song.new(name: name, album: album)
+    song.save
   end
 
 end
